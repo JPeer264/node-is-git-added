2f1a2fb308139ea18f610e902dc381d3abf65f4c
JPeer264 <jan.oster94@gmail.com>
2017-05-06 21:42:58 +0200

    Test: change generated files, moving assertions to bottom

--- test.js
+++ test.js
@@ -10,14 +10,17 @@ const isGitRequired = require('./');
 const randomString = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10);
 
 test.serial('check if something is added in process.cwd() - manipulated', (t) => {
-  fs.writeFileSync(`${randomString}.js`, 'console.log();');
-  execa.sync('git', ['add', `${randomString}.js`]);
+  fs.writeFileSync(`test_generated_${randomString}.js`, 'console.log();');
+  execa.sync('git', ['add', `test_generated_${randomString}.js`]);
 
-  t.true(isGit());
-  t.true(isGitRequired());
+  const isGitValue = isGit();
+  const isGitRequiredValue = isGitRequired();
 
-  execa.sync('git', ['reset', 'HEAD', `${randomString}.js`]);
-  fs.removeSync(`${randomString}.js`);
+  execa.sync('git', ['reset', 'HEAD', `test_generated_${randomString}.js`]);
+  fs.removeSync(`test_generated_${randomString}.js`);
+
+  t.true(isGitValue);
+  t.true(isGitRequiredValue);
 });
 
 test('check if something is added in process.cwd()', (t) => {
